14932a626a78300cae0ac19baeccb73db0c0f536
natsuyuu <natsuyuu@ubuntu.(none)>
2013-07-11 06:49:47 -0700

    test3

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
- var buffer = fs.readFile('index.html');
+var buffer = new Buffer(fs.readFileSync('index.html'),'utf-8');
+
 response.send(buffer.toString());
 });
 
